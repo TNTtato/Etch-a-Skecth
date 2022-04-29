e97f84ba54278a90e4db93e6f1c5a6eb1480e58a
gustavo <penarandagustavo5@gmail.com>
2022-04-29 16:58:29 -0500

    draw while holding click

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const gridCont = document.getElementById('grid-container');
-
-let cont = 0;
+//createin 16x16 grid
 for (let row = 1; row <= 16; row++) {
   for (let column = 1; column <= 16; column++) {
     const cellElement = document.createElement('div');
     cellElement.setAttribute('class', 'cell');
     gridCont.appendChild(cellElement);
-    cont++;
   }
 }
-console.log(cont);
+
+
+function fillCells(){
+  fullCells.forEach(cell => {
+    cell.addEventListener('mousemove', function() {
+      if(gridClicked) {
+      cell.classList.add('hovered');
+      }
+    });
+    cell.addEventListener('click', function() {
+      cell.classList.add('hovered');
+    });
+  });
+}
+
+
+const fullCells = document.querySelectorAll('.cell');
+let gridClicked = false;
+
+gridCont.addEventListener('mousedown', function() {
+  gridClicked = true;
+  fillCells();
+});
+
+gridCont.addEventListener('mouseup', function(){
+  gridClicked = false;
+  fillCells();
+});
+
+
