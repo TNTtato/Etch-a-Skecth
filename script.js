520628da6169f3f8a9350b581133a58e54c0686b
gustavo <penarandagustavo5@gmail.com>
2022-05-02 15:54:43 -0500

    add color change and grid size via variable

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
+const DEFAULT_SIZE = 64;
+const DEFAULT_COLOR = '#000000';
+
+let gridSize = DEFAULT_SIZE;
+let cellColor = DEFAULT_COLOR;
+
 const gridCont = document.getElementById('grid-container');
-//createin 16x16 grid
-for (let row = 1; row <= 16; row++) {
-  for (let column = 1; column <= 16; column++) {
-    const cellElement = document.createElement('div');
-    cellElement.setAttribute('class', 'cell');
-    gridCont.appendChild(cellElement);
+const colorPicker = document.getElementById('color-picker');
+
+function createGrid(gridSize) {
+  gridCont.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);
+  grid-template-rows: repeat(${gridSize}, 1fr);`;
+  for (let row = 1; row <= gridSize; row++) {
+    for (let column = 1; column <= gridSize; column++) {
+      const cellElement = document.createElement('div');
+      cellElement.setAttribute('class', 'cell');
+      gridCont.appendChild(cellElement);
+    }
   }
 }
+
+function pickCellColor() {
+  colorPicker.addEventListener('change', function(e) {
+    cellColor = e.target.value;
+    console.log(cellColor);
+  }, false);
+  console.log(cellColor);
+  //return cellColor
+}
+
+createGrid(gridSize);
+pickCellColor();
 
 
 function fillCells(){
   fullCells.forEach(cell => {
     cell.addEventListener('mousemove', function() {
       if(gridClicked) {
-      cell.classList.add('hovered');
+        cell.style.cssText = `background-color: ${cellColor};`;
       }
     });
     cell.addEventListener('click', function() {
-      cell.classList.add('hovered');
+      cell.style.cssText = `background-color: ${cellColor};`;
     });
   });
 }
